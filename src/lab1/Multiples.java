012db3b42a48ea83d10ab0d9c0128234431562ad
Admin <admin@Admins-MacBook-Pro.local>
2025-09-08 19:08:12 -0400

    refactored to extract helper method

---
+++ src/lab1/Multiples.java
@@ -0,0 +1,26 @@
+package lab1;
+
+public class Multiples {
+    public static void main(String[] args) {
+        int count = multiples(1000, 3, 5);
+        System.out.println(count);
+        }
+    static int multiples(int i, int k, int l) {
+        int count = 0;
+        for (int j = 1; j < i; j++) {
+            boolean divisibleBy3 = j % k == 0;
+            boolean divisibleBy5 = j % l == 0;
+
+            if (divisibleBy3 || divisibleBy5) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+
+
+    }
+
+
+
