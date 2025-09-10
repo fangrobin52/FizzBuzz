3500df3df9fd38449c95fb70a1843b695b221808
admin <robin.fang@mail.utoronto.ca>
2025-09-10 15:57:07 -0400

    refactored to extract helper method

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -5,13 +5,13 @@ public class Multiples {
         int count = multiples(1000, 3, 5);
         System.out.println(count);
         }
-    public static int multiples(int i, int k, int l) {
+        public static int multiples(int i, int a, int b) {
         int count = 0;
         for (int j = 1; j < i; j++) {
-            boolean divisibleBy3 = j % k == 0;
-            boolean divisibleBy5 = j % l == 0;
+            boolean divisibleBya = j % a == 0;
+            boolean divisibleByb = j % b == 0;
 
-            if (divisibleBy3 || divisibleBy5) {
+            if (divisibleBya || divisibleByb) {
                 count++;
             }
         }
